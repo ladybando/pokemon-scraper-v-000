b5348955c6ec73381edb9de39152632281dccc7d
Ayana Bando <ayanaflax@yahoo.com>
2018-09-06 02:59:17 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,26 +1,25 @@
-require 'pry'
 class Pokemon
-  
-  attr_accessor :name, :type, :db, :id, :hp
-  
-  def initialize(id:, name:, type:, db:, hp:  nil)
+
+attr_accessor :id, :name, :type, :hp, :db
+
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id = id
     @name = name
     @type = type
-    @db = db
-    @id = id
     @hp = hp
+    @db = db
   end
-  
+
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)",name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
-  
+
   def self.find(id, db)
-    pokemon= db.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1],type: pokemon[2], db: db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
-    
-    def alter_hp(new_hp, db)
-      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-    end
+
+  def alter_hp(new_hp, db)
+  db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+  end
 end
