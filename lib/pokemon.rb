f0ff5c151f889d860c6378dc4351ab6d175ed839
Ayana Bando <ayanaflax@yahoo.com>
2018-09-06 01:53:37 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,6 +9,7 @@ class Pokemon
     @name = name
     @type = type
     @db = db
+<<<<<<< HEAD
    end
   
   def self.save(name, type, db)
@@ -24,6 +25,23 @@ class Pokemon
   
   def alter_hp(new_hp = 60, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+=======
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def self.find(id, db)
+   
+    pokemon = db.execute("SELECT * FROM Pokemon WHERE id = ?", [id]).first
+    Pokemon.new(pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+>>>>>>> fa6a272aabba596553ce0b98e2d73468d9036b1f
   end
 end
 
